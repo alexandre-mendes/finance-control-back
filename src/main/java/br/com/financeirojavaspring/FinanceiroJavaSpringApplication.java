26538743a997c7657e9c6185f727405dae4fc43c
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-06-22 20:15:39 -0300

    Criando projeto

---
+++ src/main/java/br/com/financeirojavaspring/FinanceiroJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeirojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/financeirojavaspring/FinanceiroJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financeirojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

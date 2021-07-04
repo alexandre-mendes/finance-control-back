8929a7dab6fa44907e4cb9187715c924c5cbffe1
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-07-04 10:20:08 -0300

    Removendo metodos que deixaram de ser utilizados do Repository

--- src/main/java/br/com/financeirojavaspring/repository/InvitationRepository.java
+++ src/main/java/br/com/financeirojavaspring/repository/InvitationRepository.java
@@ -1,9 +1,7 @@
 package br.com.financeirojavaspring.repository;
 
 import br.com.financeirojavaspring.model.Invitation;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
-  Optional<Invitation> findByUuid(String uuid);
 }
--- src/main/java/br/com/financeirojavaspring/repository/UserRepository.java
+++ src/main/java/br/com/financeirojavaspring/repository/UserRepository.java
@@ -4,11 +4,6 @@ import br.com.financeirojavaspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUuid(String uuid);
-    Optional<User> findByUsername(String username);
 }

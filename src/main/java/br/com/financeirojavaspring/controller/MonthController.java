443dd75c13fb3b40e1b22a42c19f8df8f43233e7
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-07-04 14:49:57 -0300

    Adicionando MonthController

---
+++ src/main/java/br/com/financeirojavaspring/controller/MonthController.java
@@ -0,0 +1,26 @@
+package br.com.financeirojavaspring.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import java.time.LocalDate;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/mes")
+@Api(value = "Mes Controller")
+public class MonthController {
+
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(path = "/atual")
+  @ApiOperation(value = "Retorna o mês atual.", authorizations = {@Authorization(value = "Bearer")})
+  public Integer findCurrentMonth() {
+    return LocalDate.now().getMonthValue();
+  }
+}

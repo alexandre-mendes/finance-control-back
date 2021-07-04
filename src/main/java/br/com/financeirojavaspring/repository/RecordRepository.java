8af646e168559c2a1dd607356bae00dccbba7788
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-07-04 10:22:01 -0300

    Adicionando paginação na consulta.

--- src/main/java/br/com/financeirojavaspring/repository/RecordRepository.java
+++ src/main/java/br/com/financeirojavaspring/repository/RecordRepository.java
@@ -1,10 +1,15 @@
 package br.com.financeirojavaspring.repository;
 
 import br.com.financeirojavaspring.model.Record;
-import java.util.List;
+import java.time.LocalDate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-  List<Record> findAllByWalletUuid(String uuid);
+  Page<Record> findAllfindAllByWalletUuidAndDeadlineBetween(String uuidWallet,
+      LocalDate firstMonth,
+      LocalDate lastMonth,
+      Pageable pageable);
 }

ac15148a1ffb609f55c444095d1ba47b87b267fb
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-07-24 17:36:34 -0300

    Corrigindo view.

--- src/main/resources/static/scripts/V1.0__script.sql
+++ src/main/resources/static/scripts/V1.0__script.sql
@@ -12,4 +12,14 @@ left join financeiro_record_creditor record
 on wallet.id_wallet  = record.id_wallet
 where wallet.type_wallet = 'CREDITOR'
 and record.date_receivement <= now()
-group by wallet.id_wallet;
+group by wallet.id_wallet
+union
+select
+wallet.id_wallet as idWallet,
+wallet.uuid,
+wallet.title,
+wallet.type_wallet as typeWallet,
+wallet.id_account as idAccount,
+wallet.day_wallet as dayWallet,
+0 as value
+from financeiro_wallet wallet where type_wallet = 'CREDITOR' and id_wallet not in (select distinct id_wallet from financeiro_record_creditor);

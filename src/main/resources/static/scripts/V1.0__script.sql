59617c4d51b86009088e81e65308f2adbc50c9af
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-08-28 17:41:08 -0300

    Revertendo script de view.

--- src/main/resources/static/scripts/V1.0__script.sql
+++ src/main/resources/static/scripts/V1.0__script.sql
@@ -1,36 +1,12 @@
-create or replace view financeiro_wallet_vw as
+create or replace view vw_wallet_creditor as
 select
-wallet.id_wallet,
+wallet.id_wallet as idWallet,
 wallet.uuid,
 wallet.title,
-wallet.type_wallet,
-wallet.id_account,
-wallet.day_wallet,
-coalesce(sum(record.value), 0) as value,
-coalesce(sum(CASE WHEN record.paid is true THEN record.value ELSE 0 END), 0) as value_paid,
-coalesce(extract(month from record.date_deadline), 0) as month_wallet
-from financeiro_wallet wallet
-left join financeiro_record_debtor record on wallet.id_wallet = record.id_wallet
-where
-wallet.type_wallet = 'DEBTOR'
-group  by
-wallet.id_wallet,
-wallet.uuid,
-wallet.title,
-wallet.type_wallet,
-wallet.day_wallet,
-extract(month from record.date_deadline)
-union
-select
-wallet.id_wallet,
-wallet.uuid,
-wallet.title,
-wallet.type_wallet,
-wallet.id_account,
-wallet.day_wallet,
-coalesce(sum(CASE WHEN record.date_transaction <= now() THEN record.value ELSE 0 END), 0) as value,
-0 as value_paid,
-0 as month_wallet
+wallet.type_wallet as typeWallet,
+wallet.id_account as idAccount,
+wallet.day_wallet as dayWallet,
+coalesce(sum(CASE WHEN record.date_transaction <= now() THEN record.value ELSE 0 END), 0) as value
 from financeiro_wallet wallet
 left join financeiro_record_creditor record
 on wallet.id_wallet  = record.id_wallet
@@ -38,13 +14,11 @@ where wallet.type_wallet = 'CREDITOR'
 group by wallet.id_wallet
 union
 select
-wallet.id_wallet,
+wallet.id_wallet as idWallet,
 wallet.uuid,
 wallet.title,
-wallet.type_wallet,
-wallet.id_account,
-wallet.day_wallet,
-0 as value,
-0 as value_paid,
-0 as month_wallet
-from financeiro_wallet wallet where type_wallet = 'CREDITOR' and id_wallet not in (select distinct id_wallet from financeiro_record_creditor)
+wallet.type_wallet as typeWallet,
+wallet.id_account as idAccount,
+wallet.day_wallet as dayWallet,
+0 as value
+from financeiro_wallet wallet where type_wallet = 'CREDITOR' and id_wallet not in (select distinct id_wallet from financeiro_record_creditor);

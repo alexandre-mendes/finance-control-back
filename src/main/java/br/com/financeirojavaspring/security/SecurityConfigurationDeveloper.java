a38856858337aef1b55e0561b697a2c8e2a567d8
Alexandre Mendes <alexandremendes1005@gmail.com>
2021-11-02 09:21:06 -0300

    Ajustando profiles de segurança

--- src/main/java/br/com/financeirojavaspring/security/SecurityConfigurationDeveloper.java
+++ src/main/java/br/com/financeirojavaspring/security/SecurityConfigurationDeveloper.java
@@ -1,33 +1,25 @@
 package br.com.financeirojavaspring.security;
 
-import br.com.financeirojavaspring.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
-@Profile("default")
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class SecurityConfigurationDeveloper extends WebSecurityConfigurerAdapter {
 
     private final AuthenticationService authenticationService;
-    private final TokenService tokenService;
-    private final UserRepository userRepository;
 
-    public SecurityConfiguration(AuthenticationService authenticationService, TokenService tokenService, UserRepository userRepository) {
+    public SecurityConfigurationDeveloper(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
-        this.tokenService = tokenService;
-        this.userRepository = userRepository;
     }
 
     @Override
@@ -46,19 +38,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/auth").permitAll()
-                .antMatchers("/user",
-                    "/user/activation",
-                    "/v2/api-docs",
-                    "/configuration/ui",
-                    "/swagger-resources/**",
-                    "/configuration/security",
-                    "/swagger-ui.html",
-                    "/webjars/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
-                .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new AuthenticationTokenFilter(tokenService, userRepository), UsernamePasswordAuthenticationFilter.class);
+                .and().csrf().disable();
     }
 
     //Configurações de recursos estaticos (js, css, html, imagens e etc)
